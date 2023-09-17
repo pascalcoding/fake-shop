fa81e5671517deaf97df330a723a41ba75445c5d
Pascal Thiel <pascalt0604@protonmail.com>
2023-09-17 17:36:50 +0200

    Basic card implementation

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+function Card({ productName, description, price }) {
+  const [amount, setAmount] = useState(0);
+  return (
+    <div className="card">
+      <h3>{productName}</h3>
+      <p>{description}</p>
+      <div>{price}</div>
+    </div>
+  );
+}
+
+Card.PropTypes = {
+  productName: PropTypes.string,
+  description: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Card.defaultProps = {
+  productName: 'Example',
+  description: 'Example Description',
+  price: 1.1,
+};
+
+export default Card;
