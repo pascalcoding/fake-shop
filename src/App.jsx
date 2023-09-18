bdd2a5e3dd7559e25500665574be1952eb41868d
Pascal Thiel <pascalt0604@protonmail.com>
2023-09-18 10:30:43 +0200

    Added fetch for products

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import NavBar from './components/NavBar';
 import Home from './components/pages/Home';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [products, setProdcuts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const reponse = await fetch(
+        'https://fakestoreapi.com/products/category/electronics'
+      );
+      const data = await reponse.json();
+      setProdcuts(() => data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <NavBar />
