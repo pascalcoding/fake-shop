de96057fa965c9c447320279d21f1daa5a361ff1
Pascal Thiel <pascalt0604@protonmail.com>
2023-09-19 14:25:06 +0200

    Fixed paragraph

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
           <div>
             <h2>Your Tech Haven - Elevate Your Electronics Experience</h2>
             <p>
-              Welcome to <b>Fake Electronics Store</b> – your premier
-              electronics destination! We're your go-to source for cutting-edge
-              tech, offering everything from smartphones to gaming consoles. Our
+              Welcome to <b>Fake Electronics Shop</b> – your premier electronics
+              destination! We're your go-to source for cutting-edge tech,
+              offering everything from smartphones to gaming consoles. Our
               commitment to top brands and superior service ensures you stay
               connected and ahead in the world of electronics. Welcome to the
-              future of tech shopping; welcome to [Store Name]
+              future of tech shopping; welcome to Fake Electronics Shop!
             </p>
           </div>
           <div className="product-showcase"></div>
